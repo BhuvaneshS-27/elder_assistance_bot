540e62db6c606d3e3f0abfc78e6289aca0f7e73a
Bhuvanesh S <bhuvanesh.au2027@gmail.com>
2026-07-22 12:27:13 +0530

    removed secrets.py

--- secrets.py
+++
@@ -1,5 +0,0 @@
-# secrets.py — DO NOT COMMIT THIS FILE TO GIT
-# Add "secrets.py" to your .gitignore
-
-TELEGRAM_BOT_TOKEN = "8845462344:AAF6qfeC43Opur-OhuVLIcYruotZmLbqawM"
-TELEGRAM_CHAT_ID = "6108133253"
